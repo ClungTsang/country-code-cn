0c786e14864bd2ce5c63f83f99acff8ea91f4823
曾楚龙 <shawn1440982358@163.com>
2023-02-09 09:24:33 +0800

    更新逻辑代码

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-import { initCountry } from './utils/initCountry.js';
-initCountry()
+import countryFull from "country-data";
+import countryCn from './utils/country-cn.js'
+
+const continents = countryFull.continents
+const initData = () => {
+  let list = []
+  for (const key in continents) {
+    if (Object.hasOwnProperty.call(continents, key)) {
+      const continentName = continents[key].name; // 洲名称
+      const countryCodeList = continents[key].countries; // 代码列表
+      list[continentName] = []
+      countryCodeList.forEach(code => {
+        const countryData = countryCn.find(target => {
+          return target.ab == code
+        })
+        if (countryData) {
+          list[continentName].push({
+            name: continentName,
+            ...countryData,
+          })
+        }
+      })
+    }
+  }
+  return list
+}
+export { initData }
